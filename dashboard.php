a232952239655ce42e01d897b94b640daee3650f
Ramunas Daukas <ramunas.daukas@gmail.com>
2020-11-14 22:43:27 +0200

    Added artuiicle ist to article managmen

--- dashboard.php
+++ dashboard.php
@@ -27,47 +27,60 @@
   	<div class="uk-section uk-container">
         <h2>Dashboard</h2>
         <p>Hello <?php echo $User->name; ?>, you registered at <?php echo $User->reg_time; ?></p>
+        <ul class="uk-subnav uk-subnav-pill" uk-switcher>
+            <li><a href="#">Email and Name</a></li>
+            <li><a href="#">Password</a></li>
+        </ul>
 
-        <form class="uk-form-stacked js-user-update">
-            <h2>Update user info</h2>
-            <div class="uk-margin">
-                <label class="uk-form-label" for="form-stacked-text">Email</label>
-                <div class="uk-form-controls">
-                    <input class="uk-input email" id="form-stacked-text" type="email" required='required' value="<?php echo $User->email;  ?>" placeholder="email@email.com">
-                </div>
-            </div>
-            <div class="uk-margin">
-                <label class="uk-form-label" for="form-stacked-text">Name</label>
-                <div class="uk-form-controls">
-                    <input class="uk-input name" id="form-stacked-text" type="text" required='required' value="<?php echo $User->name;  ?>" placeholder="Name">
-                </div>
-            </div>
-            <div class="uk-margin uk-alert uk-alert-danger js-error" style='display: none;'></div>
-            <div class="uk-margin uk-alert uk-alert-success js-message" style='display: none;'></div>
-            <div class="uk-margin">
-                <button class="uk-button uk-button-default" type="submit">Change</button>
-            </div>
-        </form>
-        <form class="uk-form-stacked js-change-password">
-            <h2>Change password</h2>
-            <div class="uk-margin">
-                <label class="uk-form-label" for="form-stacked-text">Current Password</label>
-                <div class="uk-form-controls">
-                    <input class="uk-input current-password" id="form-stacked-text" type="Password" required='required' placeholder="Current Password">
-                </div>
-            </div>
-            <div class="uk-margin">
-                <label class="uk-form-label" for="form-stacked-text">New password</label>
-                <div class="uk-form-controls">
-                    <input class="uk-input password" id="form-stacked-text" type="password" required='required' placeholder="New password">
-                </div>
-            </div>
-            <div class="uk-margin uk-alert uk-alert-danger js-error" style='display: none;'></div>
-            <div class="uk-margin uk-alert uk-alert-success js-message" style='display: none;'></div>
-            <div class="uk-margin">
-                <button class="uk-button uk-button-default" type="submit">Change</button>
-            </div>
-        </form>
+        <ul class="uk-switcher uk-margin">
+            <li>
+                <form class="uk-form-stacked js-user-update">
+                    <h2>Update user info</h2>
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-stacked-text">Email</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input email" id="form-stacked-text" type="email" required='required' value="<?php echo $User->email;  ?>" placeholder="email@email.com">
+                        </div>
+                    </div>
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-stacked-text">Name</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input name" id="form-stacked-text" type="text" required='required' value="<?php echo $User->name;  ?>" placeholder="Name">
+                        </div>
+                    </div>
+                    <div class="uk-margin uk-alert uk-alert-danger js-error" style='display: none;'></div>
+                    <div class="uk-margin uk-alert uk-alert-success js-message" style='display: none;'></div>
+                    <div class="uk-margin">
+                        <button class="uk-button uk-button-default" type="submit">Change</button>
+                    </div>
+                </form>
+            </li>
+            <li>
+                <form class="uk-form-stacked js-change-password">
+                    <h2>Change password</h2>
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-stacked-text">Current Password</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input current-password" id="form-stacked-text" type="Password" required='required' placeholder="Current Password">
+                        </div>
+                    </div>
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-stacked-text">New password</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input password" id="form-stacked-text" type="password" required='required' placeholder="New password">
+                        </div>
+                    </div>
+                    <div class="uk-margin uk-alert uk-alert-danger js-error" style='display: none;'></div>
+                    <div class="uk-margin uk-alert uk-alert-success js-message" style='display: none;'></div>
+                    <div class="uk-margin">
+                        <button class="uk-button uk-button-default" type="submit">Change</button>
+                    </div>
+                </form>
+            </li>
+        </ul>
+
+
+
   	</div>
   	<?php require_once "inc/footer.php"; ?> 
   </body>
--- managearticle.php
+++ managearticle.php
@@ -28,27 +28,74 @@ $User = new User($_SESSION['user_id']);
 <div class="uk-section uk-container">
     <h2>Article managment</h2>
     <p>Hello <?php echo $User->email; ?>, you registered at <?php echo $User->reg_time; ?></p>
-    <form class="article">
-        <fieldset class="uk-fieldset">
+    <ul class="uk-subnav uk-subnav-pill" uk-switcher>
+        <li><a href="#">New article</a></li>
+        <li><a href="#">Edit article</a></li>
+        <li><a href="#">Delete article</a></li>
+    </ul>
 
-            <legend class="uk-legend">Legend</legend>
+    <ul class="uk-switcher uk-margin">
+        <li>
+            <form class="article">
+                <fieldset class="uk-fieldset">
 
-            <div class="uk-margin">
-                <input class="uk-input title" required='required' type="text" placeholder="Input">
-            </div>
+                    <legend class="uk-legend">Legend</legend>
 
-            <div class="uk-margin">
-                <textarea class="uk-textarea article-text" required='required' rows="10" placeholder="Textarea"></textarea>
-            </div>
+                    <div class="uk-margin">
+                        <input class="uk-input title" required='required' type="text" placeholder="Input">
+                    </div>
 
-            <div class="uk-margin uk-alert uk-alert-danger js-error" style='display: none;'></div>
+                    <div class="uk-margin">
+                        <textarea class="uk-textarea article-text" required='required' rows="10" placeholder="Textarea"></textarea>
+                    </div>
 
-            <div class="uk-margin">
-                <button class="uk-button uk-button-default" type="submit">Post article</button>
-            </div>
+                    <div class="uk-margin uk-alert uk-alert-danger js-error" style='display: none;'></div>
 
-        </fieldset>
-    </form>
+                    <div class="uk-margin">
+                        <button class="uk-button uk-button-default" type="submit">Post article</button>
+                    </div>
+
+                </fieldset>
+            </form>
+        </li>
+        <li>
+            <form class="update-article">
+                <fieldset class="uk-fieldset">
+
+                    <legend class="uk-legend">Legend</legend>
+
+                    <div class="uk-margin">
+                        <input class="uk-input title" required='required' type="text" placeholder="Input">
+                    </div>
+
+                    <div class="uk-margin">
+                        <textarea class="uk-textarea article-text" required='required' rows="10" placeholder="Textarea"></textarea>
+                    </div>
+
+                    <div class="uk-margin uk-alert uk-alert-danger js-error" style='display: none;'></div>
+
+                    <div class="uk-margin">
+                        <button class="uk-button uk-button-default" type="submit">Post article</button>
+                    </div>
+
+                </fieldset>
+            </form>
+        </li>
+        <li>
+            <ul class="uk-list">
+
+
+            <?php
+            $arrValues = Article::getArticles();
+            foreach ($arrValues as $row){
+                echo  '<li>' ,  $row[1] , "</li>";
+
+            }
+            ?>
+            </ul>
+        </li>
+    </ul>
+
 
 </div>
 
